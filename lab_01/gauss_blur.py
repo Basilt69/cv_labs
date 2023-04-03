f4a3c702b3c1608b0ccff5b46700d8e9ec916958
Basil Tkachenko <baziltkachenko@gmail.com>
2023-04-03 10:12:54 +0300

    lab 1 - gaussian blur v.3

--- lab_01/gauss_blur.py
+++ lab_01/gauss_blur.py
@@ -20,6 +20,56 @@ def show_cols():
     return c1, c2, dim, sig
 
 
+def normalize(x, mu, sd):
+    return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x-mu) / sd,2) / 2)
+
+
+def gaussian_kernel(size, sigma=1):
+    kernel_1d = np.linspace(-(size // 2), size // 2, size)
+    for i in range(size):
+        kernel_1d[1] = normalize(kernel_1d[1], 0, sigma)
+    kernel_2d = np.outer(kernel_1d.T, kernel_1d.T)
+
+    kernel_2d *= 1.0 / kernel_2d.max()
+
+    return kernel_2d
+
+
+def convolution(image, kernel, column, average=False):
+    with column:
+        st.write(f"Image size:{image.shape}")
+        st.write(f"Kernel size: {kernel.shape}")
+
+        image_row, image_col = image.shape
+        kernel_row, kernel_col = kernel.shape
+
+        output = np.zeros(image.shape)
+
+        pad_height = int((kernel_row - 1) / 2)
+        pad_width = int((kernel_col - 1) / 2)
+
+        padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
+
+        padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
+
+        for row in range(image_row):
+            for col in range(image_col):
+                output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
+                if average:
+                    output[row, col] /= kernel.shape[0] * kernel.shape[1]
+
+        st.write(f"Size of the output image: {output.shape}")
+
+        output_img = Image.fromarray((np.uint8(output)))
+
+    return output_img
+
+
+def gaussian_blur(image, kernel_size, sigma, col):
+    kernel = gaussian_kernel(kernel_size, sigma=sigma)
+    return convolution(image, kernel, col, average=True)
+
+
 def main():
     st.markdown("## Laboratory work №1")
     st.markdown("### **Title**: Gaussian blur")
@@ -55,8 +105,12 @@ def main():
         with c2:
             st.image(res, width=300)
 
+    elif func[:1] == "2":
+        c1, c2, dimension, sigma = show_cols()
+        res = gaussian_blur(gray_image, dimension, sigma, c1)
 
-
+        with c2:
+            st.image(res, width=300)
 
 
 if __name__ == "__main__":
