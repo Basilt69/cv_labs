d6ece0137af509c0683d7dcc2d60a7ea621c44de
Basil Tkachenko <baziltkachenko@gmail.com>
2023-04-03 10:37:13 +0300

    lab 1 - gaussian blur v.4

--- lab_01/gauss_blur.py
+++ lab_01/gauss_blur.py
@@ -5,17 +5,16 @@ import cv2
 from utils.utils import validate_url, uploader, get_image, FILE_TYPES
 from PIL import Image
 
-URL = "https://yandex.ru/images/search?from=tabbar&img_url=http%3A%2F%2Fnationalvanguard.org%2Fwp-content%2Fuploads%2F20" \
-      "17%2F09%2FLL-Blog_Sorens_Kantian-Liberalism_1200-1024x576.jpg&lr=213&pos=12&rpt=simage&text=Kant"
+URL = "https://globalgovernanceforum.org/wp-content/uploads/2020/11/immanuel-kant.jpg"
 
 
 def show_cols():
     # input of parameters for OpenCV.GaussianBlur
     c1, c2 = st.columns(2)
     with c1:
-        dim = st.number_input("Kernel size:", min_value=1, max_value=99, value=3, step=2)
+        dim = st.number_input("Kernel size:", min_value=1, max_value=99, value=5, step=2)
     with c2:
-        sig = st.number_input("Sigma:", min_value=1, max_value=99, value=3, step=2)
+        sig = st.number_input("Sigma:", min_value=1, max_value=99, value=8, step=1)
 
     return c1, c2, dim, sig
 
