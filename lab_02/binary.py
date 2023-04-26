100d600bf3fc91bbe3733e7c8ed62ab5aa2f6a32
Basil Tkachenko <baziltkachenko@gmail.com>
2023-04-26 10:48:53 +0300

    lab 3 - comments added

--- lab_02/binary.py
+++ lab_02/binary.py
@@ -62,35 +62,45 @@ def otsu_binary(img):
 
 
 def bradley_binary(img, t):
+    # default window size is round(cols/8)
     s = np.round(img.shape[1] / 8)
 
+    # compute integral image
     int_img = np.cumsum(np.cumsum(img, axis=1), axis=0)
 
+    # define grid of points
     (rows, cols) = img.shape[:2]
     (X, Y) = np.meshgrid(np.arange(cols), np.arange(rows))
 
+    # make into 1D grid of coordinates for easier access
     X = X.ravel()
     Y = Y.ravel()
 
+    # ensure s is even so that are able to index into the image properly
     s = s + np.mod(s, 2)
 
+    # access the four corners of each neighbourhood
     x1 = X - s / 2
     x2 = X + s / 2
     y1 = Y - s / 2
     y2 = Y + s / 2
 
+    # ensures no coordinates are out of bounds
     x1[x1 < 0] = 0
     x2[x2 >= cols] = cols - 1
     y1[y1 < 0] = 0
     y2[y2 >= rows] = rows - 1
 
+    # ensures coordinates are integer
     x1 = x1.astype(int)
     x2 = x2.astype(int)
     y1 = y1.astype(int)
     y2 = y2.astype(int)
 
+    # count how many pixels are in each neighbourhood
     count = (x2 - x1) * (y2 - y1)
 
+    # compute the row and column coordinates to access each corner of the neighbourhood for the integral image
     f1_x = x2
     f1_y = y2
     f2_x = x2
@@ -102,11 +112,14 @@ def bradley_binary(img, t):
     f4_x = f3_x
     f4_y = f2_y
 
+    # compute areas of each window
     sums = int_img[f1_y, f1_x] - int_img[f2_y, f2_x] - int_img[f3_y, f3_x] + int_img[f4_y, f4_x]
 
+    # compute thresholded image and reshape into a 2D grid
     out = np.ones(rows * cols, dtype=bool)
     out[img.ravel() * count <= sums * (100.0 - t) / 100.0] = False
 
+    # also convert back to uibt8
     out = 255 * np.reshape(out, (rows, cols)).astype(np.uint8)
 
     return out
